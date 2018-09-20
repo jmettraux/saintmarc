85cc9e37f09c45a905f41593604d7b892ea4e481
John Mettraux <jmettraux@gmail.com>
2018-09-20 17:18:23 +0900

    Scaffold spec/

---
+++ spec/parse_spec.rb
@@ -0,0 +1,17 @@
+
+#
+# spec'ing saintmarc
+#
+# Thu Sep 20 17:02:59 JST 2018
+#
+
+describe 'SaintMarc' do
+
+  describe '.parse()' do
+
+    it 'returns null if it cannot parse'
+    it 'parses a markdown string and returns a tree'
+    it 'ignores raw HTML'
+  end
+end
+
---
+++ spec/spec_helpers.rb
@@ -0,0 +1,28 @@
+
+#
+# Specifying saintmarc
+#
+# Thu Sep 20 17:00:15 JST 2018
+#
+
+require 'pp'
+require 'execjs'
+
+
+module Helpers
+
+  def js(s)
+
+    ExecJS
+      .compile(File.read('../jaabro/src/jaabro.js'))
+      .exec(s)
+  end
+end
+
+
+RSpec.configure do |c|
+
+  c.alias_example_to(:they)
+  c.include(Helpers)
+end
+
