829c172bebb59a29f850a300d6170ab3da70aa19
John Mettraux <jmettraux@gmail.com>
2020-04-05 08:00:55 +0900

    Show the error line in source.js

--- spec/spec_helpers.rb
+++ spec/spec_helpers.rb
@@ -24,6 +24,25 @@ module Helpers
     ExecJS
       .compile(File.read('spec/source.js'))
       .exec(s)
+
+  rescue ExecJS::ProgramError => err
+
+    m = err.backtrace[0].match(/:(\d+):(\d+)/)
+    l = m[1].to_i
+    c = m[2].to_i
+
+    lines = File.readlines('spec/source.js')
+    l0 = [ 0, l - 14 ].max
+    print "[90m"
+    puts '-' * 80
+    lines[l0..l - 1].each { |l| puts l }
+    puts (' ' * c) + "[97m^-- " + err.inspect
+    print "[90m"
+    lines[l, 14].each { |l| puts l }
+    puts '-' * 80
+    print "[0;0m"
+
+    raise
   end
 end
 
